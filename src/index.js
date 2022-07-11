da398300d1c672656b78fbae5df26d215e793fa4
junvely <junvely97@gmail.com>
2022-07-12 00:33:25 +0900

    :recycle: Refactoring : service파일 안에 네트워크 통신하는 클래스(youtube.js)로직을 따로 관리, app의 props으로 전달하여 호출하기

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import "@fortawesome/fontawesome-free/js/all.js";
+import Youtube from "./service/youtube";
 
+const youtube = new Youtube("AIzaSyD3Y3NpgBRs7TjOFCbYWGOStHeM31U7dvA%20");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <App />
+    <App youtube={youtube} />
   </>
 );
