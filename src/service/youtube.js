3a612b766081c464dc7aa8500deecc03d100e5ef
junvely <junvely97@gmail.com>
2022-07-14 22:38:52 +0900

    :recycle: Refactoring : fetch APIs > Axios 라이브러리로 변경

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,28 +1,32 @@
 class Youtube {
-  constructor(key) {
-    this.key = key;
-    this.requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=3&key=${this.key}`,
-      this.requestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items;
+    const response = await this.youtube.get("videos", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
   }
 
   async search(input) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=${input}&type=video&key=${this.key}`,
-      this.requestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 25,
+        type: "video",
+        q: input,
+      },
+    });
+    return response.data.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
   }
 }
 
